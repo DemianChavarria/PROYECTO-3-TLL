b4b0d4af6f1e3df2df875c46ad31c5869004ba82
Bibliasaurio02 <d.ramirez.15@estudiantec.cr>
2026-06-26 11:44:40 -0600

    prueba

---
+++ Documentacion/Programa/Clase_Grupo.py
@@ -0,0 +1,164 @@
+from Clase_Partido import Partido 
+
+class Grupo :
+
+
+
+#Objetivo: Método constructor de la clase Grupo
+
+#Entrada: Nombre del grupo
+
+#Salida: Un objeto tal construido de la clase Grupo
+
+#Restricciones: nombre_grupo debe ser un string no vacío
+
+    def __init__(self, nombre_grupo):
+
+        
+
+        self.__nombre_grupo = nombre_grupo
+
+        self.__equipos = []
+
+        self.__partidos = []
+
+
+
+
+#Objetivo: Método que agrega un equipo a la lista de equipos del grupo
+
+#Entrada: equipo (objeto de la clase Equipo), es una selección de un país que participa en el grupo
+
+#Salida: Se agrega el equipo a la lista de equipos del grupo
+
+#Restricciones: El equipo no puede ya estar en la lista del grupo, y debe tener máximo 3 selecciones para poder añadírsele una más | retorna un error sino.
+
+    def agregar_equipo(self, equipo):
+#Valida que no sea de =+4 el grupo ya.
+        if len(self.__equipos) == 4:
+
+            return f"Grupo ya tiene 4 selecciones"
+        
+#Validar no repetidos
+        for seleccion in self.__equipos: 
+
+            
+
+            if (equipo.codigo_equipo == seleccion.codigo_equipo) or (equipo.pais.nombre_pais == seleccion.pais.nombre_pais):
+
+                return f"Selección del país ya está en el grupo"
+            
+#Acción real de añadir selección al grupo
+
+        self.__equipos.append(equipo)
+
+
+
+#Método para crear los partidos necesarios:
+
+#Objetivo: Generar los 6 partidos de la fase de grupos (formato todos contra todos).
+
+#Entrada: No recibe parámetros externos (utiliza la lista self.__equipos).
+
+#Salida: Se crean y agregan 6 objetos Partido a la lista self.__partidos.
+
+#Restricciones: El grupo debe tener exactamente 4 equipos para generar los partidos correctamente.
+
+def generar_partidos(self):
+    
+    # Validación defensiva
+    if len(self.__equipos) != 4:
+        return "Error: El grupo no tiene 4 equipos para generar los partidos."
+
+    # Ciclos anidados para emparejar sin repetir
+    for i in range(len(self.__equipos)):
+        for j in range(i + 1, len(self.__equipos)):
+            
+            # Asignar local y visitante
+            local = self.__equipos[i]
+            visitante = self.__equipos[j]
+            
+            
+            partido = Partido(local, visitante, "2026-06-15")
+            
+            # Guardar en la lista del grupo
+            self.__partidos.append(partido)
+
+
+#Objetivo: Calcular la tabla de puntos de los partidos.
+
+#Entrada: El mismo objeto.
+
+#Salida: La tabla con valores numericos que representan los puntos de cada seleccion por sus jugadores.
+
+#Restricciones: Deben haber partidos en la lista para poder calcular puntos
+
+def calcular_tabla(self):
+
+    if len(self.__partidos) == 0:
+
+        return f"No hay partidos para calcular la tabla de puntos."
+# self.__equipos | tipo: lista, contiene: objetos de la clase Seleccion, quién la usa: Grupo.
+# puntosPorJugador | tipo: diccionario, contiene: claves (string codigo_equipo) y valores (int puntos), quién la usa: Grupo.
+    puntosPorJugador = {}
+
+    for equipo in self.__equipos:
+
+        puntosPorJugador[equipo.codigo_equipo] = 0
+
+
+
+    for partido in self.__partidos:
+
+
+#Si el equipo 1 gana, se le asignan 3 puntos al equipo 1, si empatan se le asigna 1 punto a cada equipo, y si gana el equipo 2 se le asignan 3 puntos al equipo 2.
+        if partido.goles_1 > partido.goles_2:
+
+            puntosPorJugador[partido.equipo_1.codigo_equipo] += 3
+#Si el equipo 1 y el equipo 2 empatan, se le asigna 1 punto a cada equipo.
+        if partido.goles_1 == partido.goles_2:
+
+            puntosPorJugador[partido.equipo_1.codigo_equipo] +=1
+
+            puntosPorJugador[partido.equipo_2.codigo_equipo] +=1
+#Si el equipo 2 gana, se le asignan 3 puntos al equipo 2.
+        if partido.goles_1 < partido.goles_2:
+
+            puntosPorJugador[partido.equipo_2.codigo_equipo] += 3
+        
+
+#Método para ordenar la tabla de puntos de los equipos en el grupo:
+
+#Objetivo: Ordenar la tabla de puntos de los equipos en el grupo de mayor a menor.
+
+#Entrada: La tabla de puntos calculada previamente.
+
+#Salida: La tabla de puntos ordenada de mayor a menor.
+
+#Restricciones: La tabla de puntos debe estar calculada previamente.
+
+def ordenar_tabla(self, puntosPorJugador):
+
+    for equipo in self.__equipos:
+
+        puntos_de_equipo = puntosPorJugador[equipo.codigo_equipo]
+
+    for i in range(len(self.__equipos)):
+
+        for j in range(i + 1, len(self.__equipos)):
+
+            equipo_1 = self.__equipos[i]
+
+            equipo_2 = self.__equipos[j]
+
+            temporal = self.__equipos[i]
+
+
+            if equipo_1.[puntos_de_equipo] > equipo_2.[puntos_de_equipo]:
+
+                self.__equipos[i] = self.__equipos[j]
+
+            if equipo_1.[puntos_de_equipo] < equipo_2.[puntos_de_equipo]:
+
+                self.__equipos[j] = temporal
+        
